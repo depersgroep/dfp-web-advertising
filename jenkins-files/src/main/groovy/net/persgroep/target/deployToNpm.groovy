6248e42f8aeea1a5eb5fc9c9983ad85ed3e8f85c
Senne Croughs <senne.croughs@gmail.com>
2018-10-10 10:07:59 +0200

    LAT-1068 use postversion and version hooks

--- jenkins-files/src/main/groovy/net/persgroep/target/deployToNpm.groovy
+++ jenkins-files/src/main/groovy/net/persgroep/target/deployToNpm.groovy
@@ -14,43 +14,25 @@ node('java-1.8') {
 
     try {
 
-        stage('Install dependencies'){
-            sh 'npm cache clean --force'
-            sh 'npm i'
-        }
-
-        stage('Bump version'){
+        stage('Bump version') {
             println("chosen semver type:" + env.VersionBump)
-            sh 'npm version ' + env.VersionBump + ' --no-git-tag-version -f -m "Bumped to a new ' + env.VersionBump +' version: %s"'
-        }
 
-        stage('Execute unit tests'){
             withCredentials([
                     string(credentialsId: 'BROWSERSTACK_KEY', variable: 'BROWSERSTACK_KEY'),
                     string(credentialsId: 'BROWSERSTACK_USR', variable: 'BROWSERSTACK_USR')
             ]) {
-                sh 'npm run test-browserstack'
+                sh 'npm version ' + env.VersionBump + ' -f -m "Bumped to a new ' + env.VersionBump + ' version: %s"'
             }
         }
 
-        stage('Build script with new version number'){
-                //sh 'npm run jenkins-build'
-                //sh 'git add .'
-                //sh 'git commit -am "Build new version"'
-        }
-
-        stage('Create git tag'){
-            //general.runGitCommand('git tag -a v1.4 -m "my version 1.4"')
-        }
-
         stage('Push new version to GitHub'){
-            //general.runGitCommand('git push origin ' + env.Branch)
-            //general.runGitCommand('git push origin --tags')
+            general.runGitCommand('git push origin ' + env.Branch)
+            general.runGitCommand('git push origin --tags')
         }
 
         stage('Push new version to NPM'){
             withNPM(npmrcConfig: 'NpmJsConfigFile') {
-                //sh 'npm publish --access public'
+                sh 'npm publish --access public'
             }
         }
 
